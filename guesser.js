07a79e4159e05a4a0e4cc40352034f1b34aeae93
magicspell <magicspellrel@gmai.com>
2024-03-02 17:20:38 -0800

    initial commit

---
+++ guesser.js
@@ -0,0 +1,50 @@
+function randInt(max) {
+    return Math.floor(Math.random() * max);
+}
+
+samples = [];
+cur_sample_id = 0;
+window.onload = function() {
+    var result = null;
+    var xmlhttp = new XMLHttpRequest();
+    xmlhttp.open("GET", "data.json", false);
+    xmlhttp.send();
+    if (xmlhttp.status==200) {
+        result = JSON.parse(xmlhttp.responseText);
+    }
+    samples = result["samples"];
+    getNewSample();
+};
+
+function getNewSample() {
+    cur_sample_id = randInt(samples.length)
+    document.getElementById("song_video").style.display = "none";
+    update_video();
+};
+
+function update_video() {
+    vid_src = "https://www.youtube.com/embed/".concat(samples[cur_sample_id]["sampleURL"]);
+    document.getElementById("sample_video").src = vid_src;
+}
+
+function display_song() {
+    song_video = document.getElementById("song_video"); 
+    song_video.style.display = "block";
+    song_video.src = "https://www.youtube.com/embed/".concat(samples[cur_sample_id]["songURL"])
+}
+
+document.getElementById("submit").addEventListener("click", function () {
+    guess = document.getElementById('guess_input').value;
+    if (guess.toLowerCase() === samples[cur_sample_id]["name"].toLowerCase()) {
+        display_song();
+        document.getElementById('guess_input').value = "";
+    }
+});
+
+document.getElementById("guess_input").addEventListener("keypress", function(event) {
+    if (event.key === "Enter") {
+        document.getElementById("submit").click();
+    }
+});
+
+document.getElementById("get_new").addEventListener("click", getNewSample);
